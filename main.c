2b8031e3df96e8f973befe5cf66abb5174497c95
Kei Sugano <kei.sugano@h-t.co.jp>
2020-05-07 23:05:29 +0900

    basic listup of files success

--- main.c
+++ main.c
@@ -1,8 +1,51 @@
 #define _GNU_SOURCE
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-int get_maximum_line_length_in_file(char * file_path) {
+int get_max_line_length_in_directory(char * directory_path) {
+    DIR    *dirp;
+    struct dirent* dent;
+    struct stat info;
+
+    dirp = opendir(directory_path);
+    if (dirp == NULL) {
+         printf ("Cannot open directory '%s'\n", directory_path);
+         return 1;
+     }
+    do {
+        dent = readdir(dirp);
+        if (dent == NULL) {
+            printf ("Cannot open directory '%s'\n", directory_path);
+            return 1;
+        }
+        if(dent){
+           if (stat(directory_path, &info) == -1) {
+               perror("stat");
+               exit(EXIT_FAILURE);
+           } 
+        }
+        switch (info.st_mode & S_IFMT) {
+            case S_IFBLK:  printf("block device\n");            break;
+            case S_IFCHR:  printf("character device\n");        break;
+            case S_IFDIR:  printf("dir ");                      break;
+            case S_IFIFO:  printf("FIFO/pipe\n");               break;
+            case S_IFLNK:  printf("lnk ");                      break;
+            case S_IFREG:  printf("reg ");                      break;
+            case S_IFSOCK: printf("socket\n");                  break;
+            default:       printf("unknown?\n");                break;
+        }
+        printf("%s \n", dent->d_name);
+    } while (dent);
+    closedir(dirp);
+    // if directory, iterate the files in the dir and recursively apply this method
+    // if file, apply get_max_line_length_in_file
+    return 0;
+}
+
+int get_max_line_length_in_file(char * file_path) {
     FILE *  fp;
     char *  line = NULL;
     size_t  len  = 0;
@@ -21,6 +64,7 @@ int get_maximum_line_length_in_file(char * file_path) {
 }
 
 int main(void) {
-    get_maximum_line_length_in_file("tmp/test");
+    get_max_line_length_in_directory("/Users/kei.sugano/code/huubhr/backend");
+    //get_max_line_length_in_file("tmp/test");
     return 0;
 }
