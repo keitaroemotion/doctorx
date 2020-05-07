a7be8212fc03f7253cbb3146a7da0a0f82a95b5c
Kei Sugano <kei.sugano@h-t.co.jp>
2020-05-07 22:41:36 +0900

    subroutine defined

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int get_maximum_line_length_in_file(char * file_path) {
     FILE *  fp;
     char *  line = NULL;
     size_t  len  = 0;
     ssize_t read;
-    fp = fopen("tmp/test", "r");
+    fp = fopen(file_path, "r");
     if(fp == NULL)
         exit(EXIT_FAILURE);
     while((read = getline(&line, &len, fp)) != -1) {
@@ -17,5 +17,10 @@ int main(void) {
     fclose(fp);
     if(line)
         free(line);
-    exit(EXIT_SUCCESS);
+    return 0;
+}
+
+int main(void) {
+    get_maximum_line_length_in_file("tmp/test");
+    return 0;
 }
